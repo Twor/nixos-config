087e4052d15c4c85c87bef0deac2ef07be9657a1
Twor <i@twor.me>
2022-11-26 21:29:23 +0800

    add icalingua

--- modules/shell/git.nix
+++ modules/shell/git.nix
@@ -5,11 +5,6 @@
       userName = "Twor";
       userEmail = "i@twor.me";
       extraConfig = {
-        url = {
-          "ssh://git@github.com:Twor" = {
-            insteadOf = "https://github.com/Twor/";
-          };
-        };
         pull.rebase = true;
         rebase.autoStash = true;
       };
